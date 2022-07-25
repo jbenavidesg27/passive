018d22be44379486a8118168c0ca2184a0c6a8ca
jbenavidesg27 <jbenavidesg27@gmail.com>
2022-07-25 15:54:56 -0500

    Seagrega el reporitorio Passive

---
+++ src/main/java/com/nttdata/bootcamp/passive/PassiveApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.passive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassiveApplication.class, args);
+	}
+
+}
